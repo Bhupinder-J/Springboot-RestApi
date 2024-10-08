2c5be8dda6eb1090b26c21df230b0cab4b636d0b
Bhupinderj <sbhupinderj9721@gmail.com>
2024-10-08 08:28:11 +0530

    initial commit

---
+++ src/main/java/com/BhupinderJ/rest/webservices/restful_web_servcies/RestfulWebServciesApplication.java
@@ -0,0 +1,13 @@
+package com.BhupinderJ.rest.webservices.restful_web_servcies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServciesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BhupinderJ/rest/webservices/restful_web_servcies/RestfulWebServciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BhupinderJ.rest.webservices.restful_web_servcies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
